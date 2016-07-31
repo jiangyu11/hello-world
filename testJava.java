7441a7332a99d7b5763fbbbf005b2f2eee003dbf
mearo <527122037@qq.com>
2016-07-31 14:11:40 +0800

    commit testJava

---
+++ testJava.java
@@ -0,0 +1,23 @@
+public class testJava{
+    private String name;
+    private String address;
+    private String telephone;
+    public void setName(Stirng name){
+       this.name = name;
+    }
+    public void setAddress(String address){
+       this.address = address;
+    }
+    public void setTelephone(String telephone){
+       this.telephone = telephone;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getAddress(){
+        return this.address;
+    }
+    public String getTelephone(){
+        return this.telephone;
+    }
+}
